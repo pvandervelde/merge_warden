1f2218a9ceedda3f730ace4235c8f5833eebeee1
Petrik van der Velde <petrikvandervelde@gmail.com>
2025-04-12 23:20:49 +1200

    Adding the terraform files used to create the infrastructure in Azure

---
+++ ops/azure/variables.tf
@@ -0,0 +1,65 @@
+#
+# ENVIRONMENT
+#
+
+variable "category" {
+  default     = "mergew"
+  description = "The name of the category that all the resources are running in."
+}
+
+variable "environment" {
+  default     = "production"
+  description = "The name of the environment that all the resources are running in."
+}
+
+#
+# GITHUB
+#
+
+variable "github_app_id" {
+  description = "Github App ID"
+  type        = string
+}
+
+variable "github_app_private_key" {
+  description = "Github App Private Key"
+  type        = string
+}
+
+variable "github_webhook_secret" {
+  description = "The secret used to validate the GitHub webhook."
+  type        = string
+}
+
+#
+# LOCATION
+#
+
+variable "location" {
+  default     = "australiaeast"
+  description = "The full name of the Azure region in which the resources should be created."
+}
+
+#
+# META
+#
+
+variable "meta_git_sha" {
+  description = "The commit ID of the current commit from which the plan is being created."
+  type        = string
+}
+
+variable "meta_version" {
+  description = "The version of the infrastructure as it is being generated."
+  type        = string
+}
+
+#
+# TAGS
+#
+
+variable "tags" {
+  description = "Tags to apply to all resources created."
+  type        = map(string)
+  default     = {}
+}
