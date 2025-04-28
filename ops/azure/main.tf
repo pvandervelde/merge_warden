a259c142501346d5749a5ebdda23a7a12bbd57c4
Petrik van der Velde <petrikvandervelde@gmail.com>
2025-04-28 21:38:14 +1200

    Updating the workspace_id to be the correct one (I think)
    references #108

--- ops/azure/main.tf
+++ ops/azure/main.tf
@@ -218,7 +218,7 @@ resource "azurerm_application_insights" "appinsights" {
   # to set it to the value that Azure gave it. Otherwise terraform will try to set it to null,
   # which will result in an error.
   # see: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/application_insights#workspace_id-1
-  workspace_id = "35762962-e84b-4078-a9ca-efdb7928dfa0"
+  workspace_id = "/subscriptions/c3420a9b-5638-4c5e-9f3a-b54263bd3662/resourceGroups/ai_p-aue-tf-mergew-insights_0f848fbd-11b2-44f3-8eea-4dccb8585e69_managed/providers/Microsoft.OperationalInsights/workspaces/managed-p-aue-tf-mergew-insights-ws"
 
   tags = merge(
     local.common_tags,
